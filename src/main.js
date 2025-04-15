18f7b90d8cdf61fb32c938cb9d393fc26622575b
AllanShima <allanshinhama@gmail.com>
2025-04-15 19:38:31 -0300

    Organização e botões novos

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ const feedback = document.getElementById('feedback');
 const resultsContainer = document.getElementById('results-container');
 
 let currentAction = null;
+
 let createdInstances = [];
 let createdGroups = [];
 
@@ -62,8 +63,8 @@ function handleSubmit() {
 // Criar Instância
 function criarInstancia(data) {
     const { nomeInstancia, telefone } = data;
-    createdInstances.push({ nomeInstancia, telefone });
-    
+
+    // createdInstances.push({ nomeInstancia, telefone });
 
     const options = {
       method: 'POST',
@@ -91,13 +92,14 @@ function criarInstancia(data) {
 // Conectar Instância
 function conectarInstancia(data) {
   const { nomeInstancia } = data;
-  
-  // Verifica se a instância existe
-  const instancia = createdInstances.find(i => i.nomeInstancia === nomeInstancia);
-  if (!instancia) {
-      showFeedback(`Instância "${nomeInstancia}" não encontrada!`, 'error');
-      return; // Encerra a função se a instância não existir
-  }
+
+  // console.log(createdInstances);
+  // // Verifica se a instância existe
+  // const instancia = createdInstances.find(i => i.nomeInstancia === nomeInstancia);
+  // if (!instancia) {
+  //     showFeedback(`Instância "${nomeInstancia}" não encontrada!`, 'error');
+  //     return; // Encerra a função se a instância não existir
+  // }
 
   showFeedback(`Conectando à instância "${nomeInstancia}"...`, 'success');
 
@@ -116,8 +118,8 @@ function conectarInstancia(data) {
       })
       .then(data => {
           // Verifica se existe pairingCode na resposta
-          if (data.qrcode && data.qrcode.pairingCode) {
-              const codigoPareamento = data.qrcode.pairingCode;
+          if (data.pairingCode) {
+              const codigoPareamento = data.pairingCode;
               alert(`Código de Pareamento: ${codigoPareamento}`);
           } else {
               throw new Error('Código de pareamento não encontrado na resposta');
@@ -132,46 +134,125 @@ function conectarInstancia(data) {
 // Criar Grupo
 function criarGrupo(data) {
     const { nomeInstancia, nomeGrupo, descricao, participantes } = data;
-    const instancia = createdInstances.find(i => i.nomeInstancia === nomeInstancia);
-    if (instancia) {
-        createdGroups.push({ nomeGrupo, descricao, participantes });
-        showFeedback(`Grupo "${nomeGrupo}" criado na instância "${nomeInstancia}"!`, 'success');
-    } else {
-        showFeedback(`Instância "${nomeInstancia}" não encontrada!`, 'error');
-    }
+
+    // const instancia = createdInstances.find(i => i.nomeInstancia === nomeInstancia);
+    // if (instancia) {
+    //     createdGroups.push({ nomeGrupo, descricao, participantes });
+    //     showFeedback(`Grupo "${nomeGrupo}" criado na instância "${nomeInstancia}"!`, 'success');
+    // } else {
+    //     showFeedback(`Instância "${nomeInstancia}" não encontrada!`, 'error');
+    // }
+
+    console.log(participantes);
+
+    const options = {
+      method: 'POST',
+      headers: {
+        apikey: MY_API_KEY,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+          subject: nomeGrupo, 
+          description: descricao,
+          participants: [participantes]
+      })
+    };
+    
+    fetch(`http://localhost:8080/group/create/${nomeInstancia}`, options)
+      .then(response => response.json())
+      .then(response => console.log(response))
+      .catch(err => console.error(err));
+
 }
 
 // Desconectar Instância
-function desconectarInstancia
+
+function desconectarInstancia(data) {
+  const { nomeInstancia } = data;
+
+  const options = {method: 'DELETE', headers: {apikey: MY_API_KEY}};
+
+  fetch(`http://localhost:8080/instance/logout/${nomeInstancia}`, options)
+    .then(response => response.json())
+    .then(data => console.log(data.response))
+    .catch(err => console.error(err));
+}
 
 // Deletar Instância
 function deletarInstancia(data) {
     const { nomeInstancia } = data;
-    createdInstances = createdInstances.filter(i => i.nomeInstancia !== nomeInstancia);
-    showFeedback(`Instância "${nomeInstancia}" deletada com sucesso!`, 'success');
+
+    // createdInstances = createdInstances.filter(i => i.nomeInstancia !== nomeInstancia);
+    // showFeedback(`Instância "${nomeInstancia}" deletada com sucesso!`, 'success');
+
+    const options = {method: 'DELETE', headers: {apikey: MY_API_KEY}};
+
+    fetch(`http://localhost:8080/instance/delete/${nomeInstancia}`, options)
+      .then(response => response.json())
+      .then(data => console.log(data.response))
+      .catch(err => console.error(err));
 }
 
 // Printar Instâncias
 function printarInstancias() {
-    resultsContainer.innerHTML = createdInstances.map(inst => `
-        <div>Instância: ${inst.nomeInstancia}, Telefone: ${inst.telefone}</div>
-    `).join('');
-    showFeedback('Instâncias listadas abaixo.', 'success');
+  const options = {method: 'GET', headers: {apikey: MY_API_KEY}};
+
+  fetch('http://localhost:8080/instance/fetchInstances', options)
+      .then(response => response.json())
+      .then(response => console.log(response))
+      .catch(err => console.error(err));
+
+  // resultsContainer.innerHTML = createdInstances.map(inst => `
+  //     <div>Instância: ${inst.nomeInstancia}, Telefone: ${inst.telefone}</div>
+  // `).join('');
+  // showFeedback('Instâncias listadas abaixo.', 'success');
 }
 
 // Printar Link de Convite
-function printarLinkConvite() {
-    const linkAleatorio = `https://wa.me/${Math.random().toString(36).substr(2, 8)}`;
-    showFeedback(`Link de convite gerado: ${linkAleatorio}`, 'success');
+function printarLinkConvite(data) {
+
+    // const linkAleatorio = `https://wa.me/${Math.random().toString(36).substr(2, 8)}`;
+
+    const { nomeInstancia, grupoJID } = data;
+
+    const options = {method: 'GET', headers: {apikey: MY_API_KEY}};
+
+    fetch(`http://localhost:8080/group/inviteCode/${nomeInstancia}?groupJid=${grupoJID}`, options)
+      .then(response => response.json())
+      .then(data => {
+        // Verifica se existe inviteUrl na resposta
+        if (data.inviteUrl) {
+            const urlConvite = data.inviteUrl;
+            alert(`Link: ${urlConvite}`);
+        } else {
+            throw new Error('Link de Convite não encontrado.');
+        }
+    })
+      .catch(err => {
+        console.error(err);
+        showFeedback(`Falha ao conectar à instância: ${err.message}`, 'error');
+      });
 }
 
 // Printar Info do Grupo
-function printarInfoGrupo() {
-    resultsContainer.innerHTML = createdGroups.map(grupo => `
-        <div>Grupo: ${grupo.nomeGrupo}, Descrição: ${grupo.descricao}, Participantes: ${grupo.participantes}</div>
-    `).join('');
-    showFeedback('Informações dos grupos listadas abaixo.', 'success');
+function printarInfoGrupo(data) {
+
+    // resultsContainer.innerHTML = createdGroups.map(grupo => `
+    //     <div>Grupo: ${grupo.nomeGrupo}, Descrição: ${grupo.descricao}, Participantes: ${grupo.participantes}</div>
+    // `).join('');
+    // showFeedback('Informações dos grupos listadas abaixo.', 'success');
+
+    const { nomeInstancia, grupoJID } = data;
+
+    const options = {method: 'GET', headers: {apikey: MY_API_KEY}};
+
+    fetch(`http://localhost:8080/group/findGroupInfos/${nomeInstancia}?groupJid=${grupoJID}`, options)
+      .then(response => response.json())
+      .then(response => console.log(response))
+      .catch(err => console.error(err));
 }
+
+// ----------------------------------------------------------------------------------------------------------------------
 
 // Eventos de Botões
 document.getElementById("criar-instancia").addEventListener("click", () => {
@@ -196,6 +277,12 @@ document.getElementById("criar-grupo").addEventListener("click", () => {
     ], criarGrupo);
 });
 
+document.getElementById("desconectar-instancia").addEventListener("click", () => {
+  openModal('Desconectar Instância', [
+      { label: 'Nome da Instância', id: 'nomeInstancia', type: 'text' }
+  ], desconectarInstancia);
+});
+
 document.getElementById("deletar-instancia").addEventListener("click", () => {
     openModal('Deletar Instância', [
         { label: 'Nome da Instância', id: 'nomeInstancia', type: 'text' }
@@ -203,8 +290,20 @@ document.getElementById("deletar-instancia").addEventListener("click", () => {
 });
 
 document.getElementById("printar-instancia").addEventListener("click", printarInstancias);
-document.getElementById("print-link-convite").addEventListener("click", printarLinkConvite);
-document.getElementById("print-info-grupo").addEventListener("click", printarInfoGrupo);
+
+document.getElementById("print-link-convite").addEventListener("click", () => {
+  openModal('Gerar Link de Convite', [
+      { label: 'Nome da Instância', id: 'nomeInstancia', type: 'text' },
+      { label: 'ID do grupo', id: 'grupoJID', type: 'text' }
+  ], printarLinkConvite);
+});
+
+document.getElementById("print-info-grupo").addEventListener("click", () => {
+  openModal('Gerar Informações do Grupo', [
+      { label: 'Nome da Instância', id: 'nomeInstancia', type: 'text' },
+      { label: 'ID do grupo', id: 'grupoJID', type: 'text' }
+  ], printarInfoGrupo);
+});
 
 // Fechar o Modal
 cancelButton.addEventListener("click", closeModal);
